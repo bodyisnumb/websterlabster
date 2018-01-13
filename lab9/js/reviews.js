a2062833ca760feea532646677669ac289b1ce24
GreedyGoblim <goblimgus@gmail.com>
2018-01-13 23:37:40 +0200

    lab9

---
+++ lab9/js/reviews.js
@@ -0,0 +1,103 @@
+/**
+ * Created by Antonina on 06.11.2017.
+ */
+
+
+window.addEventListener('load', function () {
+    function updateOnlineStatus(event) {
+        if (isOnline()) {
+            readOfflineReview();
+        }
+    }
+
+    window.addEventListener('online', updateOnlineStatus);
+    window.addEventListener('offline', updateOnlineStatus);
+});
+
+var i = 0;
+
+function isOnline() {
+    return window.navigator.onLine;
+}
+
+function addReview() {
+    if ($('#name').val() === "" || $('#text').val() === "") {
+        alert('Заповніть всі поля');
+        return false;
+    }
+    if (isOnline()) {
+        var date = new Date;
+        var author = document.getElementById('name').value;
+        var text = document.getElementById('text').value;
+        var parentElem = document.getElementById('reviews-list');
+        var out = document.createElement('div');
+        out.id = 'review';
+        out.innerHTML =
+            "<div class='container card'><br>" +
+            "   <span class='review-author'>" + author + "</span>" +
+            "   <span class='review-date'>" + date + "</span>" +
+            "   <p><br>" + text + "</p><br></div>";
+        parentElem.appendChild(out);
+        document.getElementById('form').reset();
+    } else {
+        if (useLocalStorage) {
+            var date = new Date;
+            var author = document.getElementById('name').value;
+            var text = document.getElementById('text').value;
+            i++;
+            var list = [];
+            list.push({"name": author, "text": text, "date": date});
+            localStorage.setItem('r' + i, JSON.stringify(list));
+        } else {
+            var transaction = db.transaction(["reviews"], "readwrite");
+            var store = transaction.objectStore("reviews");
+            var review = {
+                message: document.getElementById('text').value,
+                author: document.getElementById('name').value,
+                time: new Date
+            };
+            store.add(review);
+            console.log("sosoososos")
+        }
+        document.getElementById('form').reset();
+    }
+}
+
+function readOfflineReview() {
+    if (useLocalStorage) {
+        len = localStorage.length + 1;
+        for (var k = 1; k < len; k++) {
+            review = JSON.parse(localStorage.getItem('r' + k));
+            var parentElem = document.getElementById('reviews-list');
+            var out = document.createElement('div');
+            out.id = 'review';
+            out.innerHTML =
+                "<div class='container card'><br>" +
+                "   <span class='review-author'>" + review[0].name + "</span>" +
+                "   <span class='review-date'>" + review[0].date + "</span>" +
+                "   <p><br>" + review[0].text + "</p><br></div>";
+            parentElem.appendChild(out);
+            localStorage.removeItem(k);
+        }
+    } else {
+        var transaction = db.transaction(["reviews"], "readonly");
+        var store = transaction.objectStore("reviews");
+
+        store.openCursor().onsuccess = function (e) {
+            var cursor = e.target.result;
+            if (cursor) {
+                cursor.continue();
+                var parentElem = document.getElementById('reviews-list');
+                var out = document.createElement('div');
+                out.id = 'review';
+                out.innerHTML =
+                    "<div class='container card'><br>" +
+                    "   <span class='review-author'>" + cursor.value.author + "</span>" +
+                    "   <span class='review-date'>" + cursor.value.time + "</span>" +
+                    "   <p><br>" + cursor.value.message + "</p><br></div>";
+                parentElem.appendChild(out);
+            }
+        }
+    }
+}
+
