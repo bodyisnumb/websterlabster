d52f3029d09136ecbb8a933e987d916853fc91b6
GreedyGoblim <goblimgus@gmail.com>
2018-01-13 23:36:22 +0200

    lab7

---
+++ lab7/reviews.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Antonina on 06.11.2017.
+ */
+
+function addReview() {
+    if ($('#name').val() === "" || $('#text').val() === "") {
+        alert('Заповніть всі поля');
+        return false;
+    } else {
+        var date = new Date;
+        var author = document.getElementById('name').value;
+        var text = document.getElementById('text').value;
+        var parentElem = document.getElementById('reviews-list');
+        var out = document.createElement('div');
+        out.id = 'reviews';
+        out.innerHTML =
+            "<div class='container card'><br>" +
+            "   <span class='review-author'>" + author + "</span>" +
+            "   <span class='review-date'>" + date + "</span>" +
+            "   <p><br>" + text + "</p><br></div>";
+        parentElem.appendChild(out);
+        document.getElementById('form').reset();
+    }
+}
